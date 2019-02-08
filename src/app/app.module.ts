c5dbf73d948af76d300cd4a3ef5165e982f6aa2f
Faycal Hrid <faycalhrid3@gmail.com>
2019-02-08 16:07:36 +0000

    commentaire: angular with dummy server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// HttpClient module for RESTful API
+import { HttpClientModule } from '@angular/common/http';
+
+// Routing module for router service
+import { AppRoutingModule } from './app-routing.module';
+
+// Forms module
+import { FormsModule } from '@angular/forms';
+
+// Components
+import { EmployeeCreateComponent } from './employee-create/employee-create.component';
+import { EmployeesListComponent } from './employees-list/employees-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeCreateComponent,
+    EmployeesListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
